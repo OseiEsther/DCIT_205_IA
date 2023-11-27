254d7a0870031cd113b250409f13d0dd61fb4897
OseiEsther <Oseiamoakoesther2022@gmail.com>
2023-11-27 14:19:00 +0000

    feature: Added short cources caurosel

---
+++ assets/js/swippers.js
@@ -0,0 +1,11 @@
+export default function swippers() {
+  console.log('log')
+  const swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+}
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-import NavigationUtilities from './assets/js/navigation.js'
+import NavigationUtilities from "./assets/js/navigation.js";
+import swippers from "./assets/js/swippers.js";
 
-document.addEventListener('DOMContentLoaded',()=>{
-    NavigationUtilities()
-})
+document.addEventListener("DOMContentLoaded", () => {
+  NavigationUtilities();
+  // initialized swippers
+  swippers();
+});
